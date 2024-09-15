f3a8e30d39570cacc6af1ce8a1a17ebe6194a757
Md Elias Khan <124455913+Mdeliaskhan04@users.noreply.github.com>
2024-09-16 01:52:49 +0600

    Add files via upload

---
+++ Dashboard_Alert.java
@@ -0,0 +1,20 @@
+package campEssentials;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Dashboard_Alert {
+    public static void main(String[] args) {
+        WebDriverManager.firefoxdriver().clearDriverCache().setup();
+        WebDriver driver=new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://camp-essential.netlify.app/dashboard");
+        WebElement alert=driver.findElement(By.xpath("//button[text()='Delete']"));
+        alert.click();
+
+    }
+}
---
+++ FAQSection.java
@@ -0,0 +1,38 @@
+package campEssentials;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FAQSection {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.firefoxdriver().clearDriverCache().setup();
+        WebDriver driver=new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://camp-essential.netlify.app/");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,4750)", "");
+
+        WebElement header = driver.findElement(By.xpath("//h3[text()='What types of camping gear do you offer?']"));
+        header.click();
+        Thread.sleep(1500);
+        header.click();
+        Thread.sleep(1500);
+
+        WebElement header2 = driver.findElement(By.xpath("//h3[text()='Do you provide product warranties?']"));
+        header2.click();
+        Thread.sleep(1500);
+
+        WebElement header3=driver.findElement(By.xpath("//h3[text()='Can I return or exchange an item?']"));
+        header3.click();
+        Thread.sleep(1500);
+        header3.click();
+
+
+
+
+    }
+}
