7034e235cc011a0f3031481b076b9af2a973e4f1
Md Elias Khan <124455913+Mdeliaskhan04@users.noreply.github.com>
2024-09-15 23:19:09 +0600

    Add files via upload

--- Dashboard.java
+++ Dashboard.java
@@ -12,8 +12,20 @@ public class Dashboard {
         WebDriver driver=new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get("https://camp-essential.netlify.app/");
-        Thread.sleep(1500);
         WebElement dashboard=driver.findElement(By.linkText("Dashboard"));
         dashboard.click();
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+        String actualPage=driver.getTitle();
+        System.out.println(actualPage);
+        String expectedPage="Camp Essential";
+        if(actualPage.contentEquals(expectedPage))
+        {
+            System.out.println("Passed");
+        }
+        else {
+            System.out.println("Failed");
+        }
     }
 }
